ebca535a3b0a33158dd24fb1020f7c9f7b55f927
otaviozerotwo <deadplatypus@protonmail.com>
2024-01-01 20:46:23 -0300

    melhoria estrutrua do projeto, implementação back-end

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
-
-import Home from './routes/Home'
-import Chat from './routes/Chat'
-
 import './index.css'
-
-const router = createBrowserRouter([
-  {
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />
-      },
-      {
-        path: "/chat",
-        element: <Chat />
-      }
-    ]
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 )
